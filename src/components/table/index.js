e223778567103ad4304b00b579eb5610e74c8e75
Derek Lieu <dl@dereklieu.com>
2017-06-01 19:48:37 +0100

    Calculate column width, taking scrollbar into account

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Table from 'react-virtualized/dist/commonjs/Table/Table'
 import Column from 'react-virtualized/dist/commonjs/Table/Column'
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
+import getScrollBarWidth from 'get-scrollbar-width'
 
 require('react-virtualized/styles.css')
 
@@ -12,22 +13,31 @@ class TableView extends React.Component {
     return nextProps.filteredFeatures !== this.props.filteredFeatures
   }
 
+  componentWillMount () {
+    this.scrollbarWidth = getScrollBarWidth()
+  }
+
   getColumns () {
     const { filteredFeatures } = this.props
     const featureWithProperties = filteredFeatures.find(d => d.hasOwnProperty('properties'))
-    if (!featureWithProperties) { return false }
+    if (!featureWithProperties) { return [] }
     return Object.keys(featureWithProperties.properties)
   }
 
   render () {
     const { filteredFeatures } = this.props
     const rowGetter = ({ index }) => filteredFeatures[index].properties
+    const rowLength = filteredFeatures.length
     const columns = this.getColumns()
     return (
       <div style={{width: '100%', height: '100%', position: 'absolute'}}>
         <AutoSizer>
-          {({ width, height }) => (
-            <Table
+          {({ width, height }) => {
+            const totalHeight = rowHeight * rowLength
+            const overflow = totalHeight > height
+            const columnWidth = overflow ? (width - this.scrollbarWidth) / columns.length
+              : width / columns.length
+            return <Table
               disableHeader={false}
               ref='table'
               headerHeight={rowHeight}
@@ -42,12 +52,11 @@ class TableView extends React.Component {
                   label={column}
                   key={column}
                   dataKey={column}
-                  width={1}
-                  flexGrow={1}
+                  width={columnWidth}
                 />
               ))}
             </Table>
-          )}
+          }}
         </AutoSizer>
       </div>
     )
