645fdd2d7f78658fdc4756623dcce3e7d4272da1
Derek Lieu <dl@dereklieu.com>
2017-06-01 19:31:35 +0100

    Use filteredFeatures and render every column

--- src/components/table/index.js
+++ src/components/table/index.js
@@ -3,30 +3,49 @@ import Table from 'react-virtualized/dist/commonjs/Table/Table'
 import Column from 'react-virtualized/dist/commonjs/Table/Column'
 import AutoSizer from 'react-virtualized/dist/commonjs/AutoSizer'
 
+require('react-virtualized/styles.css')
+
 const rowHeight = 30
 
 class TableView extends React.Component {
+  shouldComponentUpdate (nextProps) {
+    return nextProps.filteredFeatures !== this.props.filteredFeatures
+  }
+
+  getColumns () {
+    const { filteredFeatures } = this.props
+    const featureWithProperties = filteredFeatures.find(d => d.hasOwnProperty('properties'))
+    if (!featureWithProperties) { return false }
+    return Object.keys(featureWithProperties.properties)
+  }
+
   render () {
-    console.log(this.props)
-    const { features } = this.props
-    const rowGetter = ({ index }) => features[index].properties
+    const { filteredFeatures } = this.props
+    const rowGetter = ({ index }) => filteredFeatures[index].properties
+    const columns = this.getColumns()
     return (
       <div style={{width: '100%', height: '100%', position: 'absolute'}}>
         <AutoSizer>
           {({ width, height }) => (
             <Table
+              disableHeader={false}
               ref='table'
               headerHeight={rowHeight}
               height={height}
-              rowCount={features.length}
+              rowCount={filteredFeatures.length}
               rowGetter={rowGetter}
               rowHeight={rowHeight}
               width={width}
             >
-              <Column
-                dataKey='area'
-                width={90}
-              />
+              {columns.map(column => (
+                <Column
+                  label={column}
+                  key={column}
+                  dataKey={column}
+                  width={1}
+                  flexGrow={1}
+                />
+              ))}
             </Table>
           )}
         </AutoSizer>
